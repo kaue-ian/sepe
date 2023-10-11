41a7855ee9890e15fd1265d4caf0584cb348126d
kaue-ian <131874075+kaue-ian@users.noreply.github.com>
2023-10-11 00:33:12 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+function Simular() {
+
+    //inputs and selects
+    const inicial = document.getElementById("valorApp");
+    const mensal = document.getElementById("aporteMensal");
+    const meses = document.getElementById("meses");
+    const anos = document.getElementById("anos");
+    const imposto = document.getElementById("imposto");
+    const prefix = document.getElementById("pre");
+    const posfix = document.getElementById("pos");
+    const ipca = document.getElementById("ipca");
+
+    //buttons
+
+    const btn1 = document.getElementById("btn1");
+    const btn2 = document.getElementById("btn2");
+    const btn3 = document.getElementById("btn3");
+
+}
