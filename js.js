b599e39d1979d9e42e43fdfb8b26065eac523326
Gaspatt <matheusgaspar29.if@gmail.com>
2023-10-12 15:23:07 -0300

    a

--- js.js
+++ js.js
@@ -1,19 +1,25 @@
 function Simular() {
+    
 
-    //inputs and selects
-    const inicial = document.getElementById("valorApp");
-    const mensal = document.getElementById("aporteMensal");
+    //Inputs
+    const inicial = document.getElementById("valorApp").value;
+    const prazo = document.getElementById('prazo').value;
+    const mensal = document.getElementById("aporteMensal").value;
     const meses = document.getElementById("meses");
     const anos = document.getElementById("anos");
-    const imposto = document.getElementById("imposto");
+    const imposto = document.getElementById("imposto").value;
     const prefix = document.getElementById("pre");
     const posfix = document.getElementById("pos");
     const ipca = document.getElementById("ipca");
 
-    //buttons
+    let acumulado = inicial
+    let total_acumulado = inicial + (mensal * prazo)
+
+    //botões
 
     const btn1 = document.getElementById("btn1");
     const btn2 = document.getElementById("btn2");
     const btn3 = document.getElementById("btn3");
 
+    alert(prazo)
 }
